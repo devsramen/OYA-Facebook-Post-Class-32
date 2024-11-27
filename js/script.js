fd28a498aabe0546cc3a2e4393e15c54e47d3dd6
Ramen Biswas <133987263+devsramen@users.noreply.github.com>
2024-11-27 15:17:28 +0600

    Update UI design at 27-11-2024_1517.

---
+++ js/script.js
@@ -0,0 +1,81 @@
+const postData = [
+    {
+        userImg: "./images/userPhoto.png",
+        userName: "Jon. Abrahon Linkon",
+        postTime: "16h",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque omnis aliquam voluptas quidem sit, sequi doloremque nulla quisquam, beatae illo cum ullam a modi. Doloribus consequatur, ducimus tempore laboriosam odio fugiat culpa impedit aspernatur earum eveniet obcaecati odit exercitationem facilis saepe magnam iure ut.",
+        postImage: "./images/postImage-1.jpg",
+        likedUserName: "ABM Shawon & Others",
+        reactCount: "15k",
+        commentCount: "1k",
+        shareCount : "1.5k"
+    }
+]
+
+let submitPost = document.querySelector("#submitPost")
+let postList = document.querySelector(".postList")
+submitPost.addEventListener("click",()=>{
+    postList.innerHTML = "";
+    display();
+})
+
+
+
+const display = ()=>{
+    postData.map((item)=>{
+        console.log(item);
+        postList.innerHTML += `
+            <li class="post">
+                        <div class="postHeading">
+                            <div class="postHeadingImageWrapper">
+                                <img src="${item.userImg}" alt="userPic">
+                            </div>
+                            <div>
+                                <h2 class="postUserName">${item.userName}</h2>
+                                <p class="postTime">${item.postTime}</p>
+                            </div>
+                        </div>
+                        <div class="postBody">
+                            <p class="postUserText">${item.description}</p>
+                            <div class="postBodyImageWrapper">
+                                <img src="${item.postImage}" alt="post image">
+                            </div>
+                            <div class="postBodyBottom">
+                                <div class="likeCommentCounting">
+                                    <p class="react">
+                                        <i class="fa-solid fa-heart"></i>
+                                        <i class="fa-solid fa-thumbs-up"></i>
+                                        <span>${item.likedUserName}</span>
+                                        <span id="reactCount">${item.reactCount}</span>
+                                    </p>
+                                    <div class="commentShareWrapper">
+                                        <p id="comment">
+                                            <span id="commentsCount">${item.commentCount}</span>
+                                            comments
+                                        </p>
+                                        <p id="share">
+                                            <span id="sharesCount">${item.shareCount}</span>
+                                            Shares
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="likeCommentShareIcon">
+                                    <p id="like">like</p>
+                                    <p id="comments">Comments</p>
+                                    <p id="shares">Share</p>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+        `
+    })
+}
+
+
+
+
+
+
+
+
+
